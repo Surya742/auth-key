1e60d0c1a4028c5746f51424e1effc3f9b054092
unknown <suryakumarsahani.742@gmail.com>
2020-03-26 12:37:38 +0530

    Added cookies

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
 // const md5 = require('md5');//hash secured
-const bcrypt = require('bcrypt');//salted hash secured
-const saltRounds = 10;
+// const bcrypt = require('bcrypt');//salted hash secured
+// const saltRounds = 10;
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 
 
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex",true);
 
 const app = express();
 
@@ -20,6 +25,14 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/",function(req,res){
     res.render("home");
@@ -32,66 +45,63 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
 
 const userSchema =new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 // userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.post("/register",function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        User.findOne({email:req.body.username},function(err,found){
-            if (found){
-                res.send("This email address is already taken!,try with another.");
-            }else{
-                newUser.save(function(err){
-                    if(!err){
-                        res.render("secrets");
-                    }else{
-                        res.send(err);
-                    }
-                });
-             
-            }
-        });
-          
+    User.register({username: req.body.username},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
     });
-    
-    
 });
 
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username},function(err,foundUser){
+   
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err){
         if(err){
             console.log(err);
+            res.redirect("/login");
         }else{
-            if(foundUser){
-                
-
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result == true){
-                        res.render("secrets");
-                    }else{
-                        res.send("Wrong Password!");
-                    }
-                });
-                    
-                
-            }else{
-                res.send("Wrong Username!");
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
     });
 });
